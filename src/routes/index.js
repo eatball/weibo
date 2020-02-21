d4e23a22dbe86c3a48f872012da8ca208572da44
eatball <2601120641@qq.com>
2020-02-21 21:39:01 +0800

    feat: 增加session配置

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,38 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
+    await ctx.render('index', {
+        title: 'Hello Koa 2!'
+    })
 })
 
-router.get('/profile/:username', async (ctx, next)=>{
-  const {username} = ctx.params
- ctx.body = {
-   title:'this is profile page',
-   username
- }
+router.get('/profile/:username', async (ctx, next) => {
+    const {username} = ctx.params
+    ctx.body = {
+        title: 'this is profile page',
+        username
+    }
 })
 
 router.get('/loadmore/:username/:page', async (ctx, next) => {
-      const {username, page} = ctx.params
-    ctx.body = {
-        username,
-      page
+        const {username, page} = ctx.params
+        ctx.body = {
+            username,
+            page
+        }
     }
- }
 )
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+    const session = ctx.session
+    if (session.viewNum == null) {
+        session.viewNum = 0
+    }
+    session.viewNum++
+    ctx.body = {
+        title: 'koa2 json',
+        viewNum: session.viewNum
+    }
 })
 
 module.exports = router
